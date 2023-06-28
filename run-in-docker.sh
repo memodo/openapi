605604381267cd25b3aa37920faea35b5bf044ee
Davide Diaconu <d.diaconu@memodo.it>
2023-06-28 17:30:25 +0300

    added documentation

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -11,12 +11,16 @@ mkdir -p "${maven_cache_repo}"
 # !! but using this also means we either need to setup a user with the same id, or we execute without a username and home directory.
 # !! This means we can't bind the .m2 directory to any user's directory (like /root/.m2).
 # !! We _must_ define $MAVEN_CONFIG explicitly as a location that is not /root/.m2; the user executing this may not have access to the container's user's directory.
+
+# to debug add these to the docker run command:
+# -p 5005:5005 \
+# -e JAVA_OPTS=" -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005" \
+# note: when debugger is active, the execution will not start until a listener is connected to port 5005 
+
 docker run --rm -it \
         -w /gen \
         -e GEN_DIR=/gen \
-        -p 5005:5005 \
         --add-host=host.docker.internal:host-gateway \
-        -e JAVA_OPTS=" -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005" \
         -e MAVEN_CONFIG=/var/maven/.m2 \
         -e MAVEN_OPTS="-Dhttps.protocols=TLSv1.2 -Dmaven.repo.local=/var/maven/.m2/repository -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=WARN -Dorg.slf4j.simpleLogger.showDateTime=true -Djava.awt.headless=true -Djacoco.skip=true" \
         -u "$(id -u):$(id -g)" \
